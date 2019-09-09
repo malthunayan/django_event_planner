6ca039720fbeba1d163bd730a559121206da642c
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-09-09 09:26:27 +0300

    Second commit

--- events/models.py
+++ events/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils import timezone
 
 
 class Event(models.Model):
@@ -8,7 +9,8 @@ class Event(models.Model):
 	owner = models.ForeignKey(User, on_delete=models.CASCADE)
 	content = models.TextField()
 	created_on = models.DateTimeField(auto_now_add=True)
-	tickets_available = models.PositiveIntegerField(default=1)
+	tickets_available = models.PositiveIntegerField()
+	occurance = models.DateTimeField()
 
 	def __str__(self):
 		return self.title
@@ -20,6 +22,7 @@ class BookTicket(models.Model):
 	tickets = models.PositiveIntegerField()
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	event = models.ForeignKey(Event, on_delete=models.CASCADE)
+	purchase_date = models.DateTimeField(auto_now_add=True)
 
 	def get_absolute_url(self):
 		return reverse('purchase', kwargs={'event_id':self.event.id})
--- events/views.py
+++ events/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.views import View
+from django.utils import timezone
 from .forms import UserSignup, UserLogin, EventUpdateForm, BookingForm
 from .models import Event, BookTicket
 
@@ -62,9 +63,15 @@ class Logout(View):
 		return redirect("login")
 
 def dashboard(request):
-	
+	if request.user.is_anonymous:
+		return redirect('login')
+	my_upcoming_bookings = BookTicket.objects.filter(user=request.user)
+	my_past_bookings = BookTicket.objects.filter(user=request.user)
+	my_events = Event.objects.filter(owner=request.user)
 	context = {
-
+		'my_upcoming_bookings': my_upcoming_bookings,
+		'my_past_bookings': my_past_bookings,
+		'my_events': my_events,
 	}
 	return render(request, 'dashboard.html', context)
 
@@ -85,6 +92,8 @@ def detail(request, event_id):
 	context = {
 		'event': event,
 		'form': form,
+		'start_date': event.occurance.strftime("%b. %d, %Y"),
+		'start_time': event.occurance.strftime("%I:%M %p"),
 	}
 	return render(request, 'detail.html', context)
 
